03431637234aaa44967231be7ffd64aa8836e1f7
Prog-Aleks <prog.aleksey@gmail.com>
2021-08-11 16:26:10 +0300

    2021-08-11

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 "use strict ";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+      count: numberOfFilms,
+      movies: {},
+      actor: {},
+      genres: [],
+      privat: false
+};
+
+const a = prompt('Один из просмотренных фильмов?', ''),
+      b = prompt('На сколько оценити его?', ''),
+      c = prompt('Один из просмотренных фильмов?', ''),
+      d = prompt('На сколько оценити его?', '');
 
 
-console.log('arr' + " - object")
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-
-
-
-
+console.log(personalMovieDB);
